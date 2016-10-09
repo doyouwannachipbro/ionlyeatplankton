7247164b417ba744ea2be614519cc6b11bf07a57
Beached Az <doyouwannachipbro@gmail.com>
2016-10-09 16:43:02 +0100

    Adds test for board size of 16

--- spec/ionlyeatplankton/board_spec.clj
+++ spec/ionlyeatplankton/board_spec.clj
@@ -4,4 +4,7 @@
 
 (describe "Board"
   (it "a new board of width 3 has a size 9"
-    (should= 9 (size (create-board 3)))))
+    (should= 9 (size (create-board 3))))
+
+  (it "a new board of width 4 has a size 16"
+    (should= 16 (size (create-board 4)))))
