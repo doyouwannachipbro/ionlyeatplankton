abf1ccd59664500c2df8032641a2d101915bc214
Beached Az <doyouwannachipbro@gmail.com>
2016-10-10 15:17:12 +0100

    Creates Mark type and allows board to be marked

--- spec/ionlyeatplankton/board_spec.clj
+++ spec/ionlyeatplankton/board_spec.clj
@@ -11,4 +11,11 @@
 
   (it "knows its width"
     (should= 3 (width (create-board 3))))
-  )
+
+  (it "can be marked with X"
+    (let [marked-board (to-vector (mark 3 X (create-board 3)))]
+      (should= :X (.mark (get marked-board 3)))))
+
+  (it "can be marked with O"
+    (let [marked-board (to-vector (mark 3 O (create-board 3)))]
+      (should= :O (.mark (get marked-board 3))))))
--- src/ionlyeatplankton/board.clj
+++ src/ionlyeatplankton/board.clj
@@ -1,8 +1,15 @@
 (ns ionlyeatplankton.board
   (:use [clojure.math.numeric-tower :only (sqrt) :as math]))
 
+(deftype Mark [mark])
+(def X (Mark. :X))
+(def O (Mark. :O))
+
 (defn create-board [n]
   (vec (range 1 (inc (* n n)))))
+
+(defn mark [position mark board]
+  (assoc board position mark))
 
 (defn size [board]
   (count board))
