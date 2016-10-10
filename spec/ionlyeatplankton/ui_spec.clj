5ad458e6b15a9b682e0fdbf75af1d7e7e4edcfb1
Beached Az <doyouwannachipbro@gmail.com>
2016-10-10 15:16:57 +0100

    Improves naming of test in ui

--- spec/ionlyeatplankton/ui_spec.clj
+++ spec/ionlyeatplankton/ui_spec.clj
@@ -4,6 +4,6 @@
         [ionlyeatplankton.ui]))
 
 (describe "UI"
-  (it "will show a board"
+  (it "will show a 3 x 3 board"
     (should= " 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n"
              (with-out-str (show-board (to-vector (create-board 3)))))))
