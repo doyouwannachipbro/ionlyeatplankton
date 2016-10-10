5878fef502ce89c5ce006f0e1d9b4539a7dc5e5a
Beached Az <doyouwannachipbro@gmail.com>
2016-10-10 14:13:53 +0100

    Uses declare to push private functions to the bottom of file

--- src/ionlyeatplankton/ui.clj
+++ src/ionlyeatplankton/ui.clj
@@ -2,14 +2,15 @@
   (:use [ionlyeatplankton.board]
         [clojure.string :only (join)]))
 
-(defn- show-row [row]
-  (str " " (join " | " row) " "))
-
-(defn create-rows [board]
-  (map show-row (into [] (partition (width board) board))))
+(declare show-row create-rows)
 
 (defn show-board [board]
   (doseq [row (interpose "-----------" (create-rows board))]
     (println row)))
 
+(defn- show-row [row]
+  (str " " (join " | " row) " "))
+
+(defn- create-rows [board]
+  (map show-row (into [] (partition (width board) board))))
 
