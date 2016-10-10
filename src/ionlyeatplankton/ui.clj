31053b9ab83e0e6c865c299997b45842d817d8eb
Beached Az <doyouwannachipbro@gmail.com>
2016-10-10 16:04:54 +0100

    Adds show cell method for displaying board marks

--- src/ionlyeatplankton/ui.clj
+++ src/ionlyeatplankton/ui.clj
@@ -1,16 +1,23 @@
 (ns ionlyeatplankton.ui
-  (:use [ionlyeatplankton.board]
+  (:use [ionlyeatplankton.board :as board]
         [clojure.string :only (join)]))
 
-(declare show-row create-rows)
+(declare show-row create-rows show-cell)
 
 (defn show-board [board]
   (doseq [row (interpose "-----------" (create-rows board))]
     (println row)))
 
 (defn- show-row [row]
-  (str " " (join " | " row) " "))
+    (str " " (join " | " (map show-cell row)) " "))
 
 (defn- create-rows [board]
   (map show-row (into [] (partition (width board) board))))
 
+(defn- show-cell [cell]
+  (cond
+    (= ionlyeatplankton.board.Mark (type cell))
+      (do (if (= :X (.mark cell))
+            "X"
+            "O"))
+    :else cell))
