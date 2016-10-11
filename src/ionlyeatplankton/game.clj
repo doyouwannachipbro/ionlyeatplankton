f3e6e3f295f2a894d386ed2b1f3fa6754cc1dce4
Beached Az <doyouwannachipbro@gmail.com>
2016-10-11 17:10:43 +0100

    Improves naming in game-state function

--- src/ionlyeatplankton/game.clj
+++ src/ionlyeatplankton/game.clj
@@ -12,11 +12,12 @@
     O
     X)))
 
-(defmulti game-state (fn [game] [(full? game) (type (winner game))]))
-(defmethod game-state [true ionlyeatplankton.board.Mark] [game] :winner)
-(defmethod game-state [false ionlyeatplankton.board.Mark] [game] :winner)
-(defmethod game-state [true clojure.lang.Keyword] [game] :draw)
-(defmethod game-state [false clojure.lang.Keyword] [game] :inplay)
+(defmulti game-state (fn [game] [(full? game) (winner game)]))
+
+(defmethod game-state [true (or X O)] [game] :winner)
+(defmethod game-state [false (or X O)] [game] :winner)
+(defmethod game-state [true :no-winner] [game] :draw)
+(defmethod game-state [false :no-winner] [game] :inplay)
 
 (defn- count-marks [mark game]
   (count (filter (partial = mark) game)))
