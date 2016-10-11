8f7a7c70b28196f594bb06ac27d6d88e0b8b3b73
Beached Az <doyouwannachipbro@gmail.com>
2016-10-11 10:35:39 +0100

    Removes uneccessary test

--- spec/ionlyeatplankton/board_spec.clj
+++ spec/ionlyeatplankton/board_spec.clj
@@ -3,11 +3,8 @@
         [ionlyeatplankton.board]))
 
 (describe "Board"
-  (it "a new board of width 3 has a size 9"
+  (it "knows its size"
     (should= 9 (size (create-board 3))))
-
-  (it "a new board of width 4 has a size 16"
-    (should= 16 (size (create-board 4))))
 
   (it "knows its width"
     (should= 3 (width (create-board 3))))
